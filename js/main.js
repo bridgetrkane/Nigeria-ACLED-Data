3f2c5b13d4969748029ecf29de00f273d30575f6
bridgetrkane <bridgetrkane@gmail.com>
2016-05-05 22:15:22 -0400

    Revert "edits"
    This reverts commit 3cb6fdb2208e2f7bd037d67941c370847165ef71.

--- js/main.js
+++ js/main.js
@@ -19,16 +19,14 @@ Setup
 
 var dataset = 'https://raw.githubusercontent.com/bridgetrkane/webmapping/master/Nigeria.geojson';
 var data2 = 'https://raw.githubusercontent.com/bridgetrkane/webmapping/master/NGAboundaries.geoJSON';
-var data3 = 
 var markers;
 var markers = [];
 var polygons = [];
 
-var introText = 'The Armed Conflict Location and Event Data Project (ACLED) maps points of violence across Africa. Hover over the map to learn more about the distribution of towns and provinces in Nigeria. ';
+var introText = 'The Armed Conflict Location and Event Data Project (ACLED) maps points of violence across Africa. These points reveal the distribution of violence throughout Nigeria. ';
 var introText2 = 'These slides help to give an overview of such conflicts, proiding information on actors involved, fatalities, notes, and location.';
-var t2 = 'The map above shows the distribution of points provided by ACLED.';
-var t3 = 'This purpose of these slides is to help policymakers and aid workers by highlighting violent acts throughout Nigeria in greater detail. To the right is a map showing points for each event: the darker the color, the greater the number of recorded fatalities. To get more information about a location, click on that point on the map.';
-var t4 = 'Explore clusters of violent events throughout the city. Click to get more information about each point, and reset the map using the button below.';
+var units = 'This purpose of these slides is to help policymakers and aid workers by highlighting violent acts throughout Nigeria in greater detail. To the right is a map showing points for each event: the darker the color, the greater the number of recorded fatalities. To get more information about a location, click on that point on the map.';
+var clusters = 'Explore clusters of violence throughout the city. Click to get more information about each point, and reset the map using the button below.';
 
 /* =====================
 Slides
@@ -43,73 +41,19 @@ $("#info3").text(introText2);
 $("#next-button").show();
 $("#close").hide();
 $("#legend").hide();
-$("#sidebar").hide();
-
-var defaultStyle = {
-        fillColor: '#6E5550',
-        weight: 1.5,
-        opacity: 1,
-        color: '#4D3B38',
-        fillOpacity: 0.7
-};
-
-var highlightStyle = {
-    color: '#081C1C',
-    weight: 3,
-    opacity: 0.6,
-    fillOpacity: 0.7,
-    fillColor: '#113B3B'
-};
-
-$.ajax(data2).done(function(data) {
+$("#link").hide();
+$.ajax(dataset).done(function(data) {
 var parsedData = JSON.parse(data);
 markers = L.geoJson(parsedData, {
-  onEachFeature: function(feature, layer) {
-      // Load the default style.
-      layer.setStyle(defaultStyle);
-      // Create a self-invoking function that passes in the layer
-      // and the properties associated with this particular record.
-      (function(layer, properties) {
-        // Create a mouseover event
-        layer.on("mouseover", function (e) {
-          // Change the style to the highlighted version
-          layer.setStyle(highlightStyle);
-          // Create a popup with a unique ID linked to this record
-          var popup = $("<div></div>", {
-              id: "popup-" + properties.OBJECTID,
-              css: {
-                  position: "absolute",
-                  bottom: "85px",
-                  left: "70%",
-                  zIndex: 1002,
-                  backgroundColor: "#113B3B",
-                  padding: "12px",
-                  //border: "3px radius #113B3B",
-                  'border-radius': "3px",
-                  color: '#D1D0CE',
-              }
-          });
-          // Insert a headline into that popup
-          var hed = $("<div></div>", {
-              text: 'LOCATION:  ' + properties.NAME_2 + ", " + properties.NAME_1,
-              css: {fontSize: "16px", marginBottom: "5px"}
-          }).appendTo(popup);
-          // Add the popup to the map
-          popup.appendTo("#map");
-        });
-        // Create a mouseout event that undoes the mouseover changes
-        layer.on("mouseout", function (e) {
-          // Start by reverting the style back
-          layer.setStyle(defaultStyle);
-          // And then destroying the popup
-          $("#popup-" + properties.OBJECTID).remove();
-        });
-        // Close the "anonymous" wrapper function, and call it while passing
-        // in the variables necessary to make the events work the way we want.
-      })(layer, feature.properties);
-  }
-});
-markers.addTo(map);
+  style: function(feature) {
+    return {
+      color: "#774337"};
+    },
+    pointToLayer: function(feature, latlng) {
+      return new L.CircleMarker(latlng, {radius: 7, fillOpacity: 0.75});
+    }
+  });
+  markers.addTo(map);
 });
 $("#next-button").off();
 $("#next-button").on("click", function(){
@@ -120,15 +64,12 @@ $("#next-button").on("click", function(){
 var slide2 = function(dataset){
 document.getElementById("previous-button").className = "button-previous";
 document.getElementById("next-button").className = "button-next-rest";
-$("#info").text("Event Distribution");
+$("#info").text("Fatalities");
 $("#info2").text("");
-$("#info3").text(t3);
+$("#info3").text(units);
 $("#close").hide();
 $("#legend").hide();
-$("#sidebar").hide();
-
-map.panTo(new L.LatLng(9.0765, 8.5432));
-
+$("#link").hide();
 $.ajax(dataset).done(function(data) {
 map.removeLayer(markers);
 var parsedData = JSON.parse(data);
@@ -136,25 +77,25 @@ markers = L.geoJson(parsedData, {
 style: function(feature) {
   if (feature.properties.FATALITIES <= 1)
   return {
-    color: "#A5A3A7"};
+    color: "#00B3B3"};
   if (feature.properties.FATALITIES <= 5)
   return {
-    color: "#949196"};
+    color: "#009A9A"};
   if (feature.properties.FATALITIES <= 10)
   return {
-    color: "#807C82"};
+    color: "#008080"};
   if (feature.properties.FATALITIES <= 50)
   return {
-    color: "#736F75"};
+    color: "#006767"};
   if (feature.properties.FATALITIES <= 100)
  return {
-    color: "#59575B"};
+    color: "#004D4D"};
   if (feature.properties.FATALITIES <= 600)
   return {
-    color: "#4C4A4E"};
+    color: "#003434"};
   else
   return {
-    color: "#4C4A4E"};
+    color: "#CEFFFF"};
   },
 pointToLayer: function(feature, latlng) {
 return new L.CircleMarker(latlng, {radius: 7, fillOpacity: 0.75});
@@ -176,94 +117,31 @@ $("#previous-button").on("click", function(){
 });
 };
 
-
 var slide3 = function(dataset){
 document.getElementById("previous-button").className = "button-previous";
 document.getElementById("next-button").className = "button-next-rest";
 $("#info").text("Fatalities");
 $("#info2").text("");
-$("#info3").text(t2);
+$("#info3").text(units);
 $("#close").hide();
 $("#legend").hide();
-$("#sidebar").show();
-
-map.panTo(new L.LatLng(9.0765, 11.5432));
-
-var myData;
-var myRectangle = [];
-
-$.ajax(dataset).done(function(result) {
-  map.removeLayer(markers);
-  var parsed = JSON.parse(result);
-  myData = _.chain(parsed).value();
-  layer = L.geoJson(myData, {
-  pointToLayer: function(feature, latlng) {
-  return new L.CircleMarker(latlng, {radius: 7, fillOpacity: 0.75, color: '#9C5940'});
-  },
-  filter: function(feature, layer) {
-        return feature.properties.FATALITIES >= 5;
-    },
-  onEachFeature: function (feature, layer) {
-  layer.bindPopup('<b>' + 'Fatalities: ' + feature.properties.FATALITIES + '</b><br>' + 'Location: ' + feature.properties.LOCATION + '<br>' + 'Event Type: ' + feature.properties.EVENT_TYPE);
-  }
-  }).addTo(map);
-
-
-var drawControl = new L.Control.Draw({
-draw: {
-  polyline: false,
-  polygon: false,
-  circle: false,
-  marker: false,
-  rectangle: true,
-},
-});
-
-map.addControl(drawControl);
-
-drawControl.setDrawingOptions({
-    rectangle: {
-        shapeOptions: {
-            color: '6D3E2C'
-        }
-    }
-});
-
-map.on('draw:created', function (e) {
-var type = e.layerType;
-var layer = e.layer;
-var id = L.stamp(layer);
-var shape = layer.toGeoJSON();
-var drawLayer = [];
-drawLayer.push(layer);
-
-myRectangle = {
-  "type": "FeatureCollection",
-  "features": [shape]
-};
-var Within = turf.within(myData, myRectangle);
-
-function clearSidebar() {
-  $('#shapes').empty();
+$("#link").hide();
+$.ajax(data2).done(function(data) {
+map.removeLayer(markers);
+var parsedData = JSON.parse(data);
+function style(feature) {
+    return {
+        fillColor: '#6E5550',
+        weight: 1.5,
+        opacity: 1,
+        color: '#4D3B38',
+        fillOpacity: 0.7
+    };
 }
-if(typeof layer !=='undefined') {
-  map.removeLayer(layer);
-}
-
-clearSidebar();
-
-_.each(Within.features, function(element) {
-  var template = '<div  class = "shape" id= "shape-'+element.id+'" data-id = "'+element.id+'"> <p> <b>Location: '+element.properties.LOCATION+' </b><br>Event Date: '+element.properties.EVENT_DATE+' <br>Event Type: '+element.properties.EVENT_TYPE+'</p> </div>';
-  $('#shapes').append(template);
-  $('[data-id = "'+element.id+'"]').on('click',function() {
-    var clickId = $(this).data('id');
-    var point =_.filter(Within.features,function(ob) {
-      return ob.id === clickId;
-    });
-  });
+markers = L.geoJson(parsedData, {
+  style: style
+}).addTo(map);
 });
-});
-
 $("#next-button").off();
 $("#next-button").on("click", function(){
   slide4(dataset);
@@ -271,70 +149,34 @@ $("#next-button").on("click", function(){
 $("#previous-button").off();
 $("#previous-button").on("click", function(){
   slide2(dataset);
-  map.removeLayer(layer);
-  map.removeLayer(drawControl);
-  map.addControl(removeAllControl);
-});
-});
-};
-
-var slide4 = function(dataset){
-document.getElementById("previous-button").className = "button-previous";
-document.getElementById("next-button").className = "button-next-rest";
-$("#info").text("Event Distribution");
-$("#info2").text("");
-$("#info3").text(t3);
-$("#close").hide();
-$("#legend").hide();
-$("#sidebar").hide();
-
-map.panTo(new L.LatLng(9.0765, 8.5432));
-map.removeLayer(layer);
-
-var heat = L.heatLayer([], { maxZoom: 12 }).addTo(map);
-$.ajax('js/Nigeria.json', function(data) {
-    var geojson = L.geoJson(data, {
-        onEachFeature: function(feature, layer) {
-            feature.geometry.coordinates.forEach(function(p) {
-                heat.addLatLng(L.latLng(p[0], p[1]));
-            });
-        }
-    });
-});
-};
-
-$("#next-button").off();
-$("#next-button").on("click", function(){
-  slide5(dataset);
-});
-$("#previous-button").off();
-$("#previous-button").on("click", function(){
-  slide3(dataset);
   map.removeLayer(markers);
 });
+};
 
-
-var slide5 = function(dataset) {
+var slide4 = function(dataset) {
   document.getElementById("previous-button").className = "button-previous";
   document.getElementById("next-button").className = "button-next-rest";
-  $("#info").text("Clustering of Events");
+  $("#info").text("Clustering");
   $("#info2").text("");
-  $("#info3").text(t4);
+  $("#info3").text(clusters);
   $("#close").show();
   $("#legend").hide();
-  $("#sidebar").hide();
+  $("#link").hide();
   $("#next-button").hide();
-
-  map.panTo(new L.LatLng(9.0765, 8.5432));
-
   $.ajax(dataset).done(function(data) {
     map.removeLayer(markers);
     var parsedData = JSON.parse(data);
     markers = L.markerClusterGroup();
+    /*points = L.geoJson(parsedData, {
+      pointToLayer: function(feature, latLng) {
+         return L.marker(latLng)
+        .bindPopup(feature.properties.Name);
+      }
+    });*/
     points = L.geoJson(parsedData, {
       style: function(feature) {
         return {
-          color: "#113B3B"};
+          color: "#708A92"};
         },
         pointToLayer: function(feature, latlng) {
           return new L.CircleMarker(latlng, {radius: 7, fillOpacity: 0.75})
@@ -347,7 +189,7 @@ var slide5 = function(dataset) {
   });
   var closeResults = function() {
     $("#intro").show();
-    $("#results").hide();
+    $("#reslults").hide();
     this.map.setView(new L.LatLng(9.0765, 8.5432), 6);
   };
   $("#close").click(function() {
@@ -355,9 +197,8 @@ var slide5 = function(dataset) {
   });
   $("#previous-button").off();
   $("#previous-button").on("click", function(){
-    slide4(dataset);
+    slide3(dataset);
     $("#next-button").show();
-    map.removeLayer(markers);
   });
 };
 
